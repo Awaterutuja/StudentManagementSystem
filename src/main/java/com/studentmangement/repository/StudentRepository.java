12a68291efacca4688b972e5b37b0c6686a06010
AwateRutuja <rutujaawate7@gmail.com>
2022-01-05 12:55:24 +0530

    Initial Commit

---
+++ src/main/java/com/studentmangement/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.studentmangement.repository;
+
+import com.studentmangement.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
